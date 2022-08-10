ff99e41ca923e93e090c4275af214eec69ffad65
Chris Witte <christopher.t.witte@gmail.com>
2022-08-10 09:17:35 -0400

    commit tree b64c57701a72a736b72372b71e8a343266bbcaa0

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards (names) {
+    let thanks = [];
+    for (let i = 0; i < names.length; i++) {
+        thanks[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+    }
+    return thanks
+}
+
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+        console.log(i);
+    }
+}
